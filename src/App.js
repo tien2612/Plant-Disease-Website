8ea8554a2e7da37019c6a7f32a12d6819f35a2ab
Nguyen Phuc Tien <nguyenphuctien1258@gmail.com>
2023-03-19 15:38:31 +0700

    fixed router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
 import About from "./scenes/about";
 import Images from "./scenes/images";
-// import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 
@@ -30,8 +22,8 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/" element={<About />} />
-              <Route path="/" element={<Images />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/images" element={<Images />} />
             </Routes>
           </main>
         </div>
